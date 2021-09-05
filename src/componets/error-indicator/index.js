252d2db1980b4fc6603018336f699d69613729e2
vladmishaev <vlad.mishaev.1998@gmail.com>
2021-09-05 19:12:15 +0300

    improved componets random planet

---
+++ src/componets/error-indicator/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './index.css';
+import gif from './error.gif';
+
+const Error = () => {
+    return (
+        <div className="error-indicator">
+            <div className="img-error-box">
+                <img src={gif} />
+            </div>
+            <div>
+                <h3 className="boom">BOOM!</h3>
+                <p>something has gone terribly wrong </p>
+                <p>(but we already sent droids to fix it)</p>
+            </div>
+        </div>
+    )
+}
+
+export default Error;
